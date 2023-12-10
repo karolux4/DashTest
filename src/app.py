a19acf2c78d70f7537a36815a733899e769c03d7
karolux4 <karolisbutkus4@gmail.com>
2023-12-10 15:40:31 +0200

    Added CSV visualization

--- src/app.py
+++ src/app.py
@@ -25,32 +25,28 @@ def urlDownload(urlLink):
 urlLocation = 'https://aqicn.org/data-platform/covid19/report/39374-7694ec07/'
 csvFile = urlDownload(urlLocation)
 
+# Download the newest data
+urlLocation = 'https://aqicn.org/data-platform/covid19/report/39374-7694ec07/'
+csvFile = urlDownload(urlLocation)
+
+# Define the columns to load
+meta_cols = ['Date', 'Country', 'City', 'Specie']
+main_column = 'median' # 'count', 'min', 'max', 'median', 'variance'
+selected_cols = meta_cols + [main_column]
+
+# Read the newest data file and skip the first 4 lines
+DF = pd.read_csv(csvFile, skiprows=4, usecols=selected_cols, nrows=50)
+
 app = Dash(__name__, title="DashTest")
 
 # Declare server for Heroku deployment. Needed for Procfile.
 server = app.server
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+# App layout
+app.layout = html.Div([
+    html.Div(children='My First App with Data, Graph, and Controls'),
+    html.Hr(),
+    dash_table.DataTable(data=DF.to_dict('records'), page_size=6)
 ])
 
 if __name__ == '__main__':
